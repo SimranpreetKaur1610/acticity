510604389dad70cc32b94f7847a702efc8d45c5f
Simranpreetkaur <harsim610@gmail.com>
2021-03-20 12:02:51 -0400

    initial commit

---
+++ src/com/simran/classActivity.java
@@ -0,0 +1,12 @@
+package com.simran;
+
+public class classActivity
+{
+    public static void main(String[] args)
+    {
+        String str = "hello hello how are you HELLO are there happy test you";
+        String[] words = str.split(" ");  //use the split
+        int wordCounts=1;    //Variable for getting Repeated word count
+
+    }
+}
