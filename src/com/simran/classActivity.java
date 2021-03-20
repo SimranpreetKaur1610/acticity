5ee9227542b05923d9d6cfb0f2dd1572a64a0867
Simranpreetkaur <harsim610@gmail.com>
2021-03-20 12:13:22 -0400

    added the inner and outer for loop to compare

--- src/com/simran/classActivity.java
+++ src/com/simran/classActivity.java
@@ -8,5 +8,18 @@ public class classActivity
         String[] words = str.split(" ");  //use the split
         int wordCounts=1;    //Variable for getting Repeated word count
 
+        //use outer for loop to compare
+        for (int i = 0;i<words.length;i++)
+        {
+            //use inner for loop to compare
+            for (int j =0; j<words.length;j++)
+            {
+                if(words[i] == words[j])
+                {
+                    wordCounts=wordCounts+1;
+
+                }
+            }
+        }
     }
 }
